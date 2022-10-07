b3b013afd8b7f6879474e27af962186574c194fc
Ramanjs <raman.s2662@gmail.com>
2022-10-07 15:47:10 +0530

    footer and navbar updates

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,10 +4,14 @@ import iiitdlogo from '../assets/iiitd-logo.svg';
 const Footer = () => {
   return (
     <div className="footer">
-      <img src={iiitdlogo} alt="iiitd" />
-      <div>
+      <div className="iiitd-address">
+        <img src={iiitdlogo} alt="iiitd" />
+        <span>Okhla Industrial Estate, Phase III, near Govind Puri Metro Station</span>
+        <span>New Delhi, Delhi 110020</span>
+      </div>
+      <div className="links">
         <p>Website Designed & Developed By</p>
-        <h2><a href="">Aditya Girdhar</a> & <a href="">Ramanjeet Singh</a></h2>
+        <h2><a href="https://www.linkedin.com/in/adityagirdhar/">Aditya Girdhar</a> & <a href="https://www.linkedin.com/in/ramanjeet-singh-340754229/">Ramanjeet Singh</a></h2>
       </div>
     </div>
   )
--- src/pages/Navbar.js
+++ src/pages/Navbar.js
@@ -26,7 +26,7 @@ const Navbar = (props) => {
           <li><NavLink to="/faq" onClick={() => toggleNav()} className={({ isActive }) => isActive ? "nav-active" : ""}>FAQs</NavLink></li>
           <li><NavLink to="/team" onClick={() => toggleNav()} className={({ isActive }) => isActive ? "nav-active" : ""}>Our Team</NavLink></li>
         </ul>
-        <div></div>
+        <div style={{width: '50px'}}></div>
       </div>
     </div>
   )
